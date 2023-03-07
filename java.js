d6ccd69d9def3fef0e4802744b2fe7608f4af39f
BenitezEfra <124022202+BenitezEfra@users.noreply.github.com>
2023-03-06 23:32:01 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+    var ingresodeusuario =[];
+    var resultado;
+    var palabrasDelUsuario;
+function subPalabras(){
+    
+    palabrasDelUsuario =document.getElementById("ingreso").value;
+    /*resultado = palabrasDelUsuario * 0.2;
+    palabrasDelUsuario = resultado + palabrasDelUsuario*/
+    alert("la cantidad generada es: ",palabrasDelUsuario);
+}
+
+
+
---
+++ java2.js
@@ -0,0 +1,11 @@
+function rom2(){
+    var ingresodeusuario1 =[];
+    var ventas;
+    
+    sueldobase =document.getElementById("sueldo").value;
+    ventas = sueldobase * 0.3;
+    sueldobase = sueldobase + ventas
+    alert("El dinero que obtendrá seran: ", sueldobase);
+    
+   
+   }
